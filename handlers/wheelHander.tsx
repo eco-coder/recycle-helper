b32ad15d2305c528658eca1cf6568c948c04da48
DESKTOP-7QBQJEG\yunis <luckchurch@gmail.com>
2021-09-18 22:17:13 +0900

    wheel 조절 중...

--- handlers/wheelHander.tsx
+++ handlers/wheelHander.tsx
@@ -2,19 +2,19 @@ const wheelHandlerF = (sections: any, count: number, setcount: any) => {
 
     const wheelHandler = async (e: any) => {
 
-        if (e.wheelDelta < 0 && count < sections.length - 1 && e.deltaY > 30) {
+        if (e.wheelDelta < 0 && count < sections.length - 1 && e.deltaY > 36) {
 
             let number: number = await new Promise(function (resolve, reject) {
                 setTimeout(() => {
                     resolve(count + 1)
-                }, 380);
+                }, 280);
             })
             await setcount(number);
-        } else if (e.wheelDelta > 0 && count > 0 && e.deltaY < -30) {
+        } else if (e.wheelDelta > 0 && count > 0 && e.deltaY < -36) {
             let number: number = await new Promise(function (resolve, reject) {
                 setTimeout(() => {
                     resolve(count - 1)
-                }, 380);
+                }, 280);
             })
             await setcount(number);
         }
